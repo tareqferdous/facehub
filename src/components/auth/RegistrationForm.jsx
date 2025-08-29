185e045dac1c479e03c3c85d2cd13c9082c50450
tareqferdous <16101017@uap-bd.edu>
2025-08-29 11:14:31 +0600

    user register implemented

--- src/components/auth/RegistrationForm.jsx
+++ src/components/auth/RegistrationForm.jsx
@@ -1,12 +1,10 @@
 import axios from "axios";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../hooks/useAuth";
 import Field from "../common/Field";
-const LoginForm = () => {
-  const navigate = useNavigate();
-  const { setAuth } = useAuth();
 
+const RegistrationForm = () => {
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
@@ -17,33 +15,49 @@ const LoginForm = () => {
   const submitForm = async (formData) => {
     try {
       const response = await axios.post(
-        `${import.meta.env.VITE_SERVER_BASE_URL}/auth/login`,
+        `${import.meta.env.VITE_SERVER_BASE_URL}/auth/register`,
         formData
       );
 
-      if (response.status === 200) {
-        const { user, token } = response.data;
-        if (token) {
-          const authToken = token.token;
-          const refreshToken = token.refreshToken;
-          console.log(`Login time auth token: ${authToken}`);
-          setAuth({ user, authToken, refreshToken });
-          navigate("/");
-        }
+      if (response.status === 201) {
+        navigate("/login");
       }
     } catch (error) {
       setError("root.random", {
         type: "random",
-        message: `User with email ${formData.email} is not found`,
+        message: `Something went wrong: ${error.message}`,
       });
     }
   };
-
   return (
     <form
-      className="border-b border-[#3F3F3F] pb-10 lg:pb-[60px]"
+      className="border-b border-[#3F3F3F] pb-10 lg:pb-[30px]"
       onSubmit={handleSubmit(submitForm)}
     >
+      <Field label="First Name" error={errors.firstName}>
+        <input
+          {...register("firstName", {
+            required: "First Name is Required",
+          })}
+          className={`auth-input ${
+            errors.firstName ? "border-red-500" : "border-gray-200"
+          }`}
+          type="firstName"
+          name="firstName"
+          id="firstName"
+        />
+      </Field>
+      <Field label="Last Name" error={errors.lastName}>
+        <input
+          {...register("lastName")}
+          className={`auth-input ${
+            errors.lastName ? "border-red-500" : "border-gray-200"
+          }`}
+          type="lastName"
+          name="lastName"
+          id="lastName"
+        />
+      </Field>
       <Field label="Email" error={errors.email}>
         <input
           {...register("email", { required: "Email ID is Required" })}
@@ -74,13 +88,14 @@ const LoginForm = () => {
         />
       </Field>
       <p>{errors?.root?.random?.message}</p>
-      <Field>
-        <button className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90">
-          Login
-        </button>
-      </Field>
+      <button
+        className="auth-input bg-lwsGreen font-bold text-deepDark transition-all hover:opacity-90"
+        type="submit"
+      >
+        Register
+      </button>
     </form>
   );
 };
 
-export default LoginForm;
+export default RegistrationForm;
