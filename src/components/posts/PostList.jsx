30e7d6ce915be4f35e6cfc7fb5632bb94e8b4c11
tareqferdous <16101017@uap-bd.edu>
2025-08-29 10:50:20 +0600

    user posts and post feed implemented

---
+++ src/components/posts/PostList.jsx
@@ -0,0 +1,6 @@
+import PostCard from "./PostCard";
+const PostList = ({ posts }) => {
+  return !!posts && posts.map((post) => <PostCard key={post.id} post={post} />);
+};
+
+export default PostList;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,41 @@
-import { Link } from "react-router-dom";
-import { useAuth } from "../hooks/useAuth";
+import { useEffect, useReducer } from "react";
+import { actions } from "../actions";
+import PostList from "../components/posts/PostList";
+import useAxios from "../hooks/useAxios";
+import { initialState, postReducer } from "../reducers/postReducer";
 
 const HomePage = () => {
-  const { auth } = useAuth();
+  const [state, dispatch] = useReducer(postReducer, initialState);
+  const { api } = useAxios();
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      dispatch({ type: actions.post.DATA_FETCHING });
+      try {
+        const response = await api.get(
+          `${import.meta.env.VITE_SERVER_BASE_URL}/posts`
+        );
+        if (response.status === 200) {
+          dispatch({ type: actions.post.DATA_FETCHED, data: response.data });
+        }
+      } catch (error) {
+        dispatch({ type: actions.post.DATA_FETCH_ERROR, error: error.message });
+      }
+    };
+    fetchPost();
+  }, []);
+
+  if (state?.loading) {
+    return <div> We are working...</div>;
+  }
+
+  if (state?.error) {
+    return <div> Error in fatching posts {state?.error?.message}</div>;
+  }
+
   return (
     <div>
-      HomePage
-      <Link to="/me">Go to Profile</Link>
+      <PostList posts={state?.posts} />
     </div>
   );
 };
